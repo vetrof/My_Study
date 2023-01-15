1f2cb8e9fba0d862929ea092926cb420fe2a7d4c
Vitaly Vetrof <111348000+vetrof@users.noreply.github.com>
2023-01-15 16:28:56 +0600

    add file

--- harvard_CS50python/week4_libraries/problem/adieu.py
+++ harvard_CS50python/week4_libraries/problem/adieu.py
@@ -2,17 +2,16 @@
 # Adieu, adieu, to Liesl and Friedrich
 # Adieu, adieu, to Liesl, Friedrich, and Louisa
 
-# import inflect
-# str = 'to plate house'
-# mod_str = inflect.engine().plural_noun(str, count=None)
-# print(mod_str)
+import inflect
+p = inflect.engine()
 
 # solution without using module inflect
 def main():
+
     name_list = get_name()
     # name_list = ['Liesl', 'Friedrich', 'Louisa', 'Kurt', 'Brigitta', 'Marta', 'Gretl']
     output = text_gen(name_list)
-    print(f'Adieu, adieu, to{output}')
+    print(f'Adieu, adieu, to {output}')
 
 
 def get_name():
@@ -21,30 +20,17 @@ def get_name():
         try:
             name = input('Name: ')
         except EOFError:
+            print()
             break
         else:
             name_list.append(name)
 
-    print()
     return name_list
 
 
 def text_gen(names):
-    text = ''
-    n_name = len(names)
-
-    if n_name == 1:
-        text = text + ' ' + names[0]
-        return text
-
-    if n_name > 1:
-        for name in names:
-
-            index = names.index(name)
-            text = text + ' ' + name + ","
-            if index == (n_name - 2):
-                text = text.rstrip(',') + ' and ' + names[index + 1]
-                return text
+    textout = p.join(names)
+    return textout
 
 
 if __name__ == '__main__':
