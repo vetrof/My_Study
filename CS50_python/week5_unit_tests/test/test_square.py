14e01a8de9e25cb1d79015eff2ac42230a7f22fb
Vitaly Vetrof <111348000+vetrof@users.noreply.github.com>
2023-01-16 20:55:30 +0600

    update

---
+++ CS50_python/week5_unit_tests/test/test_square.py
@@ -0,0 +1,16 @@
+from calc import square
+
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+
+ 
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+
+def test_zero():
+    assert square(0) == 0
+
